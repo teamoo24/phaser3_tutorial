20c9f6accea682bcf6fb120e124afff56196e77e
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-04-02 15:33:29 +0900

    Initial commit

---
+++ src/scenes/Level.ts
@@ -0,0 +1,45 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Level extends Phaser.Scene {
+
+	constructor() {
+		super("Level");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	editorCreate(): void {
+
+		// sky
+		this.add.image(400, 300, "sky");
+
+		// star
+		this.add.image(383, 211, "star");
+
+		this.events.emit("scene-awake");
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here
+
+	create() {
+
+		this.editorCreate();
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
