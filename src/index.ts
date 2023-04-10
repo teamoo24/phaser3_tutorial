76d08248f88eca837185f48932a44176d6fbf72d
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-04-10 14:20:52 +0900

    prefeb tutorial done

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,38 @@ import preloadAssetPackUrl from "../static/assets/preload-asset-pack.json";
 import Preload from "./scenes/Preload";
 
 class Boot extends Phaser.Scene {
+  constructor() {
+    super("Boot");
+  }
 
-    constructor() {
-        super("Boot");
-    }
+  preload() {
+    this.load.pack("pack", preloadAssetPackUrl);
+  }
 
-    preload() {
-
-        this.load.pack("pack", preloadAssetPackUrl);
-    }
-
-    create() {
-
-       this.scene.start("Preload");
-    }
+  create() {
+    this.scene.start("Preload");
+  }
 }
 
-window.addEventListener('load', function () {
-	
-	const game = new Phaser.Game({
-		width: 800,
-		height: 600,
-		backgroundColor: "#2f2f2f",
-		scale: {
-			mode: Phaser.Scale.ScaleModes.FIT,
-			autoCenter: Phaser.Scale.Center.CENTER_BOTH
-		},
-		scene: [Boot, Preload, Level]
-	});
+window.addEventListener("load", function () {
+  const game = new Phaser.Game({
+    width: 800,
+    height: 600,
+    backgroundColor: "#2f2f2f",
+    scale: {
+      mode: Phaser.Scale.ScaleModes.FIT,
+      autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+    },
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: {
+          y: 300,
+        },
+      },
+    },
+    scene: [Boot, Preload, Level],
+  });
 
-	game.scene.start("Boot");
-
+  game.scene.start("Boot");
 });
