341397a75f0657e95b28fd462c5116df0ba8c46b
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-04-11 14:05:37 +0900

    skipを変更してphaser3_tutorial_releaserを無駄に読み込まないようにする

---
+++ src/scenes/PlayerPrefab.ts
@@ -0,0 +1,42 @@
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default interface PlayerPrefab {
+
+	 body: Phaser.Physics.Arcade.Body;
+}
+
+export default class PlayerPrefab extends Phaser.Physics.Arcade.Image {
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number, texture?: string, frame?: number | string) {
+		super(scene, x ?? 65, y ?? 52, texture || "dude", frame ?? 0);
+
+		scene.physics.add.existing(this, false);
+		this.body.bounce.x = 0.2;
+		this.body.bounce.y = 0.2;
+		this.body.collideWorldBounds = true;
+		this.body.setSize(32, 48, false);
+
+		/* START-USER-CTR-CODE */
+    this.scene.events.once("scene-awake", () => this.awake());
+    /* END-USER-CTR-CODE */
+	}
+
+	public property1: string = "";
+
+	/* START-USER-CODE */
+  awake() {
+    this.play("left");
+  }
+
+  /* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
